a9ee118b5c16131da2066919ea8751c08ea52114
Ananth L <ananth.l@hotmail.com>
2015-07-22 17:12:15 +0530

    Project created with layout for first VC

---
+++ uWear/uWear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  uWear
+//
+//  Created by Ananth L on 22/07/15.
+//  Copyright (c) 2015 Ananth L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
